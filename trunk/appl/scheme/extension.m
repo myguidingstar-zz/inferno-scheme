608c4a328c709454e7c03dd498568b249b6ac0d2
blstuart <blstuart@8fd6673e-3548-11de-8ef0-0f53c1b7e458>
2012-02-09 01:56:59 +0000

    oops...spawn is not going to work that way..this should be better

--- trunk/appl/scheme/extension.m
+++ trunk/appl/scheme/extension.m
@@ -3,5 +3,5 @@ Extension: module
 	PATH: con "/dis/scheme/extension.dis";
 
 	init: fn(drawctxt: ref Draw->Context, s: Sys, sch: Scheme, c: SCell,
-		b: BuiltIn, m: Math, st: String);
+		b: BuiltIn, m: Math, st: String, bi: Bufio);
 };
